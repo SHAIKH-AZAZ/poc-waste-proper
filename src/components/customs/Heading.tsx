1585ab99ffa942907bda37754cff38f5c4bd65c5
SHAIKH-AZAZ <azazshaikh2703@gmail.com>
2025-09-26 12:25:19 +0530

    changed Header configs

--- src/components/customs/Heading.tsx
+++ src/components/customs/Heading.tsx
@@ -1,17 +1,26 @@
 "use client";
-import { LayoutTextFlip } from "@/components/ui/layout-text-flip";
-import { motion } from "motion/react";
+import { FlipWords } from "../ui/flip-words";
 
 export function HeadDemo() {
+  const words = [
+    "Buniadbyte",
+    "Bar Cutting Optimization",
+    "Dynamic Bar Cutting",
+    "Greedy Bar Cutting",
+  ];
   return (
     <div>
-      <motion.div className="relative mx-4 flex flex-col items-center gap-4 text-center sm:mx-0 sm:mb-0 sm:flex-row">
-        <LayoutTextFlip text="Welcome to "
-        
-        words={["Buniadbyte" , "Bar Cutting Optimization" , "Dynamic Bar Cutting" , "Greedy Bar Cutting"]}
-        />
-      </motion.div>
-      <p className="mt-4 text-center text-base text-shadow-neutral-600 dark:text-neutral-400 ">A prototype for waste optimzation for Bar cutting</p>
+      <p className="mt-4 text-center text-base text-shadow-neutral-600 dark:text-neutral-400 ">
+        A prototype for waste optimzation for Bar cutting
+      </p>
+
+      <div className="grid grid-col text-4xl mx-auto font-normal text-neutral-900 dark:text-neutral-800">
+        {/* <div className="grid-cols-1">
+
+        </div> */}
+        <div className="text-center">Welcome</div>
+        <FlipWords words={words} className="text-center" /> <br />
+      </div>
     </div>
   );
 }
